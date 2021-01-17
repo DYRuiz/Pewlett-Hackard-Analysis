bc7d7d2c95f6faccb3c087c0e01482d031a0b4ed
DRUiz <danieleyt@hotmail.com>
2021-01-17 00:04:04 -0800

    challege upload

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,48 @@
+-- Employee Database Challenge
+-- Deliverable 1
+SELECT emp.emp_no,
+	emp.first_name,
+	emp.last_name,
+	tl.title,
+	tl.from_date,
+	tl.to_date
+INTO retirement_titles
+FROM employees as emp
+INNER JOIN titles as tl 
+ON (emp.emp_no=tl.emp_no)
+WHERE (emp.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp.emp_no;
+	
+-- Use Distinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY emp_no, from_date DESC;
+
+-- Count of eligible retiring by title
+SELECT COUNT (ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT(ut.title) DESC;
+
+-- Deliverable 2
+SELECT DISTINCT ON (emp.emp_no) emp.emp_no,
+	emp.first_name,
+	emp.last_name,
+	emp.birth_date,
+	de.from_date,
+	de.to_date,
+	tl.title
+INTO mentorship_eligiblity
+FROM employees as emp
+INNER JOIN dept_emp as de
+ON (emp.emp_no = de.emp_no)
+INNER JOIN titles as tl
+ON (emp.emp_no=tl.emp_no)
+WHERE (emp.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp.emp_no;
